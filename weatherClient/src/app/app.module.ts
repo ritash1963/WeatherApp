8676c0d8075dce94500603b82c9cac15592d6527
ritash1963 <rita.shmidt@gmail.com>
2022-03-23 19:20:04 +0200

    test

--- weatherClient/src/app/app.module.ts
+++ weatherClient/src/app/app.module.ts
@@ -5,6 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [	
@@ -14,7 +17,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgChartsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- weatherClient/src/app/weather/weather.component.ts
+++ weatherClient/src/app/weather/weather.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
+import { WeatherVM } from '../_models/weatherVM';
+import { WeatherService } from '../_services/weather.service';
 
 @Component({
   selector: 'app-weather',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  weather: WeatherVM[];
+  tempNum: number[];
+  labelStr: string[];
+   public barChartOptions: ChartOptions = {
+    responsive: true,
+    };
+  public barChartLabels = [];
+  public barChartType: ChartType = 'line';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+  public barChartData: ChartDataset[] = [
+     { data: [], label: 'Tel Aviv' }
+   ];  
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) { }
 
   ngOnInit() {
+    this.getWeatherData();  
   }
 
+  getWeatherData() {
+    this.weatherService.getWeather().subscribe(result => {
+       this.weather = result;
+       this.tempNum = this.weather.map(a => a.weatherTemp);
+       this.barChartData[0].data = this.tempNum;
+       this.labelStr = this.weather.map(a => a.weatherDate);
+       this.barChartLabels = this.labelStr;
+       },error => {
+       console.log(error);
+       });
+    
+      }  
 }
